e7b48a71a865793ac6b49996298455085995d713
Naphop Khumchawna <nutnaphop24@gmail.com>
2026-02-16 14:55:30 +0700

    [ SPR-86ew8wabv ]  Imrprove Sync Strategy to global

--- lib/register_provider.dart
+++ lib/register_provider.dart
@@ -1,5 +1,10 @@
+import 'package:dun_diary_app/core/auth/auth_service.dart';
 import 'package:dun_diary_app/core/core_providers.dart';
+import 'package:dun_diary_app/core/network/network_info.dart';
+import 'package:dun_diary_app/core/services/sync_service.dart';
+import 'package:dun_diary_app/data/blood_pressure/repository/blood_pressure_repository.dart';
 import 'package:dun_diary_app/data/providers/repository_providers.dart';
+import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 
 List<SingleChildWidget> get appProviders {
@@ -9,5 +14,20 @@ List<SingleChildWidget> get appProviders {
 
     // 2. Shared Repositories (Data Layer)
     ...sharedRepositoryProviders,
+
+    // 3. Global Services (depends on repos + core)
+    ProxyProvider3<
+      NetworkInfo,
+      AuthService,
+      BloodPressureRepository,
+      SyncService
+    >(
+      update: (_, networkInfo, authService, repository, __) => SyncService(
+        networkInfo: networkInfo,
+        authService: authService,
+        repository: repository,
+      ),
+      lazy: false, // Create immediately
+    ),
   ];
 }
