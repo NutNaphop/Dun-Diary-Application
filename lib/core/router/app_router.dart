3cc5e4de7f6f9804d20e1e72361bdd768b21af04
Naphop Khumchawna <nutnaphop24@gmail.com>
2026-02-18 00:35:39 +0700

    [ SPR-86ew8wd73 ] Improve router to debloat

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,65 +1,17 @@
-import 'dart:io';
-
+import 'package:dun_diary_app/core/router/router_registry.dart';
 import 'package:dun_diary_app/core/services/app_logger.dart';
-import 'package:dun_diary_app/data/blood_pressure/model/bp_record.dart';
-import 'package:dun_diary_app/feature/main/presentation/main_screen.dart';
-import 'package:dun_diary_app/feature/record/presentation/record_screen.dart';
-import 'package:dun_diary_app/feature/record/presentation/resultScreen/result_screen.dart';
-import 'package:dun_diary_app/feature/setting/presentation/profileEditScreen/profile_edit_screen.dart';
-import 'package:dun_diary_app/feature/setting/presentation/profileSettingScreen/profile_setting_screen.dart';
-import 'package:dun_diary_app/feature/splash_screen/presentation/splash_screen.dart';
-import 'package:dun_diary_app/shared/widgets/ui/page/mock_page.dart';
 import 'package:flutter/material.dart';
-
-import '../constant/app_routes.dart';
 
 class AppRouter {
   static Route<dynamic> generate(RouteSettings settings) {
     AppLogger.debug('Navigate to: ${settings.name}');
-    final args = settings.arguments;
 
-    if (settings.name == AppRoutes.splash) {
-      return _buildRoute(const SplashScreen());
-    } else if (settings.name == AppRoutes.main) {
-      return _buildRoute(const MainScreen());
-    } else if (settings.name == AppRoutes.record) {
-      if (args is BPRecord) {
-        return _buildRoute(RecordScreen.createWithRecord(args));
-      }
-      return _buildRoute(RecordScreen.create());
-    } else if (settings.name == AppRoutes.mock) {
-      if (args != null) {
-        AppLogger.debug("args: ${args.toString()}");
-      }
-      return _buildRoute(const MockPage());
-    } else if (settings.name == AppRoutes.result) {
-      if (args is File) {
-        return MaterialPageRoute(
-          builder: (_) => ResultScreen.createWithArg(args),
-        );
-      }
-      return _errorRoute("Need image for this page");
-    } else if (settings.name == AppRoutes.history) {
-      return _errorRoute('History route not implemented yet');
-    } else if (settings.name == AppRoutes.setting.recovery) {
-      return _errorRoute('Recovery screen coming soon');
-    } else if (settings.name == AppRoutes.setting.tutorial) {
-      return _buildRoute(Center(child: Text("This is Tutorial Screen")));
-    } else if (settings.name == AppRoutes.setting.profile) {
-      return _buildRoute(ProfileSettingScreen.create());
-    } else if (settings.name == AppRoutes.setting.profileEdit) {
-      return _buildRoute(ProfileEditScreen.create());
-    } else {
-      return _errorRoute('No route defined for ${settings.name}');
-    }
+    final route = RouterRegistry.instance.generateRoute(settings);
+    if (route != null) return route;
+
+    return _errorRoute('No route defined for ${settings.name}');
   }
 
-  // Helper Function: ช่วยให้เขียนสั้นลง (Optional)
-  static MaterialPageRoute _buildRoute(Widget child) {
-    return MaterialPageRoute(builder: (_) => child);
-  }
-
-  // Error Page: หน้าแจ้งเตือนเวลาหลงทาง
   static Route<dynamic> _errorRoute(String message) {
     return MaterialPageRoute(
       builder: (_) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:dun_diary_app/core/constant/app_routes.dart';
 import 'package:dun_diary_app/core/constant/hive_constants.dart';
 import 'package:dun_diary_app/core/router/app_router.dart';
+import 'package:dun_diary_app/core/router/router_registry.dart';
 import 'package:dun_diary_app/core/services/navigation_service.dart';
 import 'package:dun_diary_app/core/services/snackbar_service.dart';
 import 'package:dun_diary_app/data/analyze_record/model/analyze_cache_model.dart';
 import 'package:dun_diary_app/data/analyze_record/model/analyze_result_model.dart';
 import 'package:dun_diary_app/data/blood_pressure/model/bp_record.dart';
+import 'package:dun_diary_app/feature/main/router/main_router.dart';
+import 'package:dun_diary_app/feature/record/router/record_router.dart';
+import 'package:dun_diary_app/feature/setting/router/setting_router.dart';
 import 'package:dun_diary_app/register_provider.dart';
 import 'package:dun_diary_app/shared/constant/app_strings.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -20,6 +24,7 @@ void main() async {
   await initService();
   await initHive();
   await initFirebase();
+  initRouter();
   runApp(MultiProvider(providers: appProviders, child: MyApp()));
 }
 
@@ -37,6 +42,13 @@ class MyApp extends StatelessWidget {
       onGenerateRoute: AppRouter.generate,
     );
   }
+}
+
+// init router
+void initRouter() {
+  RouterRegistry.instance.register(MainRouter());
+  RouterRegistry.instance.register(RecordRouter());
+  RouterRegistry.instance.register(SettingRouter());
 }
 
 // init service
