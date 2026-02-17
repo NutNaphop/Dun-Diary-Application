5588982dc648567bc6cdf9715ecea15c9cad0084
Naphop Khumchawna <nutnaphop24@gmail.com>
2026-02-18 02:00:35 +0700

    [ SPR-86ew8wd73 ] Implement profile edit

--- lib/feature/setting/presentation/profileEditScreen/profile_edit_viewModel.dart
+++ lib/feature/setting/presentation/profileEditScreen/profile_edit_viewModel.dart
@@ -1,3 +1,26 @@
+import 'package:dun_diary_app/core/services/app_logger.dart';
 import 'package:flutter/material.dart';
 
-class ProfileEditViewmodel extends ChangeNotifier {}
+class ProfileEditViewModel extends ChangeNotifier {
+  final TextEditingController nameController = TextEditingController();
+
+  ProfileEditViewModel() {
+    _initData();
+  }
+
+  void _initData() {
+    // TODO: Fetch user data from repository/usecase
+    nameController.text = "เอกพจน์"; // Mock data
+  }
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    super.dispose();
+  }
+
+  void saveProfile() {
+    // TODO: Implement save logic
+    AppLogger.debug("Saving name: ${nameController.text}");
+  }
+}
