5e814e1e16c132b1593d1bca1ae74c4ea6fc9f62
Naphop Khumchawna <nutnaphop24@gmail.com>
2026-02-20 23:44:36 +0700

    [ SPR-86ew8wd73 ] Done in my qrcode maybe back to improve export card[

--- lib/feature/setting/presentation/myQrCodeScreen/my_qr_code_screen.dart
+++ lib/feature/setting/presentation/myQrCodeScreen/my_qr_code_screen.dart
@@ -1,20 +1,27 @@
+import 'package:dun_diary_app/core/auth/provider/global_user_provider.dart';
 import 'package:dun_diary_app/core/services/navigation_service.dart';
+import 'package:dun_diary_app/data/user/repository/user_repository.dart';
 import 'package:dun_diary_app/feature/setting/presentation/myQrCodeScreen/my_qr_code_viewModel.dart';
 import 'package:dun_diary_app/shared/constant/app_icons.dart';
+import 'package:dun_diary_app/shared/style/color.dart';
 import 'package:dun_diary_app/shared/style/dimension.dart';
+import 'package:dun_diary_app/shared/widgets/custom/button/custom_button.dart';
 import 'package:dun_diary_app/shared/widgets/custom/img/custom_svg_widget.dart';
 import 'package:dun_diary_app/shared/widgets/custom/scaffold/custom_scaffold.dart';
 import 'package:dun_diary_app/shared/widgets/custom/scaffold/main_appbar.dart';
 import 'package:dun_diary_app/shared/widgets/custom/text/text_widget.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:screenshot/screenshot.dart';
 
 class MyQrCodeScreen extends StatefulWidget {
   const MyQrCodeScreen({super.key});
 
   static Widget create() {
     return ChangeNotifierProvider(
-      create: (_) => MyQrCodeViewmodel(),
+      create: (context) =>
+          MyQrCodeViewmodel(userRepository: context.read<UserRepository>()),
       child: const MyQrCodeScreen(),
     );
   }
@@ -26,6 +33,7 @@ class MyQrCodeScreen extends StatefulWidget {
 class _MyQrCodeScreenState extends State<MyQrCodeScreen> {
   @override
   Widget build(BuildContext context) {
+    final userName = context.select((GlobalUserProvider p) => p.displayName);
     return CustomScaffold(
       usePadding: false,
       appBar: MainAppBar(
@@ -68,6 +76,51 @@ class _MyQrCodeScreenState extends State<MyQrCodeScreen> {
             SizedBox(height: 26),
 
             // QRCODE SECTION
+            Screenshot(
+              controller: context
+                  .read<MyQrCodeViewmodel>()
+                  .screenshotController,
+              child: Center(
+                child: context.watch<MyQrCodeViewmodel>().isLoading
+                    ? const CircularProgressIndicator()
+                    : Column(
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(20),
+                              border: Border.all(
+                                color: Colors.grey.shade300,
+                                width: 2,
+                              ),
+                            ),
+                            child: QrImageView(
+                              data: context.watch<MyQrCodeViewmodel>().qrData,
+                              version: QrVersions.auto,
+                              size: 270,
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          CustomText(
+                            text: "ชื่อผู้ใช้ : ${userName}",
+                            fontSize: Dimension.fontSizes.md,
+                            fontWeight: Dimension.fontWeights.bold,
+                            color: CustomColor.gray500,
+                          ),
+                        ],
+                      ),
+              ),
+            ),
+            Spacer(),
+            SizedBox(
+              width: double.infinity,
+              child: CustomButton(
+                text: "บันทึกรูปลงเครื่อง",
+                type: CustomButtonType.fill,
+                onPressed: context.read<MyQrCodeViewmodel>().snapImage,
+              ),
+            ),
           ],
         ),
       ),
