50dfdac5d4ec18f61e3a5619c481106c467ad656
Naphop Khumchawna <nutnaphop24@gmail.com>
2026-03-04 19:10:24 +0700

    [ SPR-86ewtbjc6 ] - Add new icon and change in recovery screen

--- lib/feature/setting/data/menu_data.dart
+++ lib/feature/setting/data/menu_data.dart
@@ -50,7 +50,7 @@ class RecoveryMenuData {
         ),
         SettingItem(
           title: 'กู้คืนข้อมูลของฉัน',
-          svgPath: AppIcons.bottomNav.userCircle,
+          svgPath: AppIcons.duotone.cloudDownload,
           route: AppRoutes.setting.recoveryMyData,
           onTap: () => AppLogger.debug('Usage Guide tapped'),
         ),
