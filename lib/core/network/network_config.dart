b2c9476855c8b10039fbcdc1b6d8249d9ac496f8
Naphop Khumchawna <nutnaphop24@gmail.com>
2026-03-05 01:28:02 +0700

    remove test and manage env and add build define

--- lib/core/network/network_config.dart
+++ lib/core/network/network_config.dart
@@ -1,5 +1,8 @@
 class ClientConfig {
-  static const BASE_URL = "http://10.0.2.2:3000/api/";
+  static const BASE_URL = String.fromEnvironment(
+    "BASE_URL",
+    defaultValue: "http://10.0.2.2:3000/api/",
+  );
   static const TIMEOUT = Duration(seconds: 10);
   static const DEFAULT_HEADER = {
     "Content-Type": "application/json",
